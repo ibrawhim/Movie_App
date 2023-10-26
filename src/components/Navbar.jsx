af2f5044f26819e71cc833a414492a932a35e826
Adeniyi Ibrahim Abiodun <ibrahimabiodun069@gmail.com>
2023-10-26 12:47:30 +0100

    movie

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,15 +9,15 @@ import guy from '../assets/Images/guy.jpg'
 const Navbar = () => {
   return (
     <>
-    <div className='sticky w-full top-0 bg-red-700'>
+    <div className='sticky w-full top-0 bg-red-700 z-[10000000]'>
         <nav className='border border-red-500 flex justify-around py-1'>
-        <Link className='mt-2' to="/">Home</Link>
-        <div className='flex gap-10 mt-3'>
+        <Link className='mt-4' to="/">Home</Link>
+        <div className='flex gap-10 mt-5'>
         <Link to="/"><AiOutlineHome/></Link>
         <Link to="/"><TfiVideoClapper/></Link>
         <Link to="/"><MdOutlinePersonalVideo/></Link>
         </div>
-        <img src={guy} className='rounded-[100%] h-[45px] border border-black'  width={47} alt="" />
+        <img src={guy} className='rounded-[100%] h-[43px] border border-black'  width={45} alt="" />
         {/* <div className=''>Image</div> */}
         </nav>
     </div>
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -8,13 +8,17 @@ const Home = () => {
     const [searchMovie, setsearchMovie] = useState('')
     const [movies, setmovies] = useState([])
     const [tv, settv] = useState([])
+    const [discover, setdiscover] = useState([])
     const [select, setselect] = useState('day')
+    const [first, setfirst] = useState(second)
     // console.log(select);
     const key = '82b6a6612f5c7ebddd0064847db7ed24'
     let endpoint = `https://api.themoviedb.org/3/trending/movie/day?language=en-US&api_key=${key}`
     let endpoint2 =  `https://api.themoviedb.org/3/trending/movie/week?language=en-US&api_key=${key}`
     let endpoint3 = `https://api.themoviedb.org/3/trending/tv/day?language=en-US&api_key=${key}`
     let endpoint4 = `https://api.themoviedb.org/3/trending/tv/week?language=en-US&api_key=${key}`
+    let endpoint5 = `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc&api_key=${key}`
+    let endpoint6 = `https://api.themoviedb.org/3/search/multi?query=${searchMovie}&include_adult=false&language=en-US&page=1&api_key=${key}`
 
     const imgBaseUrl = "https://image.tmdb.org/t/p";
 
@@ -52,34 +56,42 @@ const Home = () => {
                 console.log(error);
             })
     }
+    axios.get(endpoint5)
+    .then((response)=>{
+        setdiscover(response.data.results)
+    })
+    .catch((error)=>{
+        console.log(error);
+    })
 
 
 
 
-    // const Search = () => {
-    //     axios.get(endpoint)
-    //     .then((result)=>{
-    //         console.log(result);
-    //     })
-    //     .catch((error)=>{
-    //         console.log(error);
-    //     })
-    // }
+    const Search = () => {
+        axios.get(endpoint6)
+        .then((result)=>{
+            // setmovies(result.data.results)
+            console.log(movies);
+        })
+        .catch((error)=>{
+            console.log(error);
+        })
+    }
   return (
     <>
         <div className='mt-5 mx-3 lg:mx-5'>
             <div className='lg:w-1/3 md:w-1/2 w-full flex justify-between'>
-                <div className='flex border border-2 rounded border-black w-3/4'>
-                <span className='mt-2'><GrFormSearch/></span>
-                <input type="text" onChange={(e) => setsearchMovie(e.target.value)} placeholder='Search Movies or TV Series'  className='w-full border border-white py-1'/>
+                <div className='flex border border-2 rounded border-black bg-white w-3/4'>
+                <span className='mt-2 mx-2'><GrFormSearch/></span>
+                <input type="text" onChange={(e) => setsearchMovie(e.target.value)} placeholder='Search Movies or TV Series'  className='text-black w-full border border-white py-1'/>
                 </div>
-                <button  className='bg-black text-white px-3 rounded'>Search</button>
+                <button onClick={Search} className='bg-red-700  font-bold text-white px-5 rounded'>Search</button>
             </div>
 
             <section className='my-10'>
                 <div className='flex'>
                     <h1 className='me-4'>Trending</h1>
-                    <select className='border border-black' name="" id="" onChange={(e)=>setselect(e.target.value)} value={select.day}>
+                    <select className='text-black' name="" id="" onChange={(e)=>setselect(e.target.value)} value={select.day}>
                         <option value="day">day</option>
                         <option value="week">week</option>
                     </select>
@@ -104,6 +116,17 @@ const Home = () => {
                             <div key={i} className=''>
                                 <img src={`${imgBaseUrl}/original/${items.poster_path}`} className='hover:scale-110 rounded w-full h-[70px]' alt="" />
                                 <div className='text-center'>{items.name}</div>
+                            </div>
+                        ))
+                    }
+                </div>
+                <h1 className='my-10'>Discover</h1>
+                <div className='grid lg:grid-cols-7 grid-cols-3 gap-10'>
+                    {
+                        discover.map((items,i)=>(
+                            <div key={i} className=''>
+                                <img src={`${imgBaseUrl}/original/${items.poster_path}`} className='hover:scale-110 rounded w-full h-[70px]' alt="" />
+                                <div className='text-center'>{items.original_title}</div>
                             </div>
                         ))
                     }
